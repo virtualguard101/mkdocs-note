e8952be67f724067b93ac2eef50e46e5553e9f2b
VIRTUALGUARD <virtualguard101@gmail.com>
2025-10-19 14:49:57 +0800

    chore: adjust metadata of project in

--- src/mkdocs_note/__init__.py
+++ src/mkdocs_note/__init__.py
@@ -1,9 +1,19 @@
 from datetime import datetime
 
 __author__ = "virtualguard101"
-__version__ = "2.1.0"
 __description__ = "A MkDocs plugin to add note boxes to your documentation."
 __license__ = "GPL-3.0-or-later"
 __url__ = "https://github.com/virtualguard101/mkdocs-note"
 __email__ = "virtualguard101@gmail.com"
 __copyright__ = f"Copyright {datetime.now().year} virtualguard101"
+
+try:
+    from ._version import __version__
+except ImportError:
+    # Fallback for development installations without setuptools-scm
+    try:
+        from importlib.metadata import version
+
+        __version__ = version("mkdocs-network-graph-plugin")
+    except ImportError:
+        __version__ = "unknown"
